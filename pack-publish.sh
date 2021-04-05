d7d91f9fcb44c15753ec44e819b8e4721f764c9d
lra <lra@r73.info>
2021-04-05 18:04:12 +0200

    fix script

--- pack-publish.sh
+++ pack-publish.sh
@@ -4,20 +4,22 @@ set -e -u -o pipefail
 # clean
 rm -f *.tgz index*.yaml
 
+mkdir /repo
+
 # package 
-helm package .
-PKG_NAME=$(basename $(ls *.tgz))
+helm package . -d /repo
+PKG_NAME=$(basename $(ls /repo/*.tgz))
 
 # download old yaml
-curl --insecure -u $HELM_REPO_USER:$HELM_REPO_PWD -XGET $HELM_REPO_URL/index.yaml -o index-old.yaml
+curl --insecure -u $HELM_REPO_USER:$HELM_REPO_PWD -XGET $HELM_REPO_URL/index.yaml -o /repo/index-old.yaml
 
-helm repo index . --merge index-old.yaml 
+helm repo index /repo --merge /repo/index-old.yaml 
 echo PKG_NAME=$PKG_NAME
 curl --insecure -H 'Content-type: application/octet-stream' \
-     -u $HELM_REPO_USER:$HELM_REPO_PWD --data-binary @$PKG_NAME  \
+     -u $HELM_REPO_USER:$HELM_REPO_PWD --data-binary @/repo/$PKG_NAME  \
      -XPUT $HELM_REPO_URL/$PKG_NAME 
 curl --insecure -H 'Content-type: text/x-yaml' \
-     -u $HELM_REPO_USER:$HELM_REPO_PWD --data-binary @index.yaml\
+     -u $HELM_REPO_USER:$HELM_REPO_PWD --data-binary @/repo/index.yaml\
      -XPUT  $HELM_REPO_URL/index.yaml 
 rm -f *.tgz index*.yaml
 
